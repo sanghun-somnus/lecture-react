7b547d3757fb0f337c4a3538ab30b3ef5c389b61
sanghun-somnus <somnus.btc@gmail.com>
2024-09-19 22:57:46 +0900

    lecture: 값 파생시키기, 질문 출력 및 답변 등록

--- section12/src/components/Quiz.jsx
+++ section12/src/components/Quiz.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 
+import QUESTIONS from "../data";
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prev) => [...prev, selectedAnswer]);
+  };
+
   return (
-    <main>
-      <p>Quiz</p>
-    </main>
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li
+              key={answer}
+              className='answer'
+            >
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
