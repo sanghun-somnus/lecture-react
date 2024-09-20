b4c480fe3c294e5a367a41e43fddde90d5c348d6
sanghun-somnus <somnus.btc@gmail.com>
2024-09-20 16:57:57 +0900

    lecture: 답변 셔플 & 퀴즈 로직 추가

--- section12/src/components/Quiz.jsx
+++ section12/src/components/Quiz.jsx
@@ -1,22 +1,38 @@
 import { useState } from "react";
 
 import QUESTIONS from "../data";
+import completeLogo from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  if (quizIsComplete) {
+    return (
+      <div id='summary'>
+        <img
+          src={completeLogo}
+          alt='Trophy icon'
+        />
+        <h2>quiz complete!</h2>
+      </div>
+    );
+  }
 
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prev) => [...prev, selectedAnswer]);
   };
+
+  const suffledAnswers = QUESTIONS[activeQuestionIndex].answers.sort(() => Math.random() - 0.5);
 
   return (
     <div id='quiz'>
       <div id='question'>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id='answers'>
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {suffledAnswers.map((answer) => (
             <li
               key={answer}
               className='answer'
