92579532a8c1f26bdfb4821443f87ec003627169
sanghun-somnus <somnus.btc@gmail.com>
2024-09-21 17:32:22 +0900

    lecture: 선택된 답변 강조 & 추가 State 관리

--- section12/src/components/Quiz.jsx
+++ section12/src/components/Quiz.jsx
@@ -1,15 +1,40 @@
+// hook
 import { useState, useCallback } from "react";
 
+// components
 import QuestionTimer from "./QuestionTimer";
 
+// assets
 import QUESTIONS from "../data";
 import completeLogo from "../assets/quiz-complete.png";
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex = answerState === "" ? userAnswers.length : userAnswers.length - 1;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  // event handlers
+  const handleSelectAnswer = useCallback(
+    (selectedAnswer) => {
+      setAnswerState("answered");
+      setUserAnswers((prev) => [...prev, selectedAnswer]);
+
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+        setTimeout(() => {
+          setAnswerState("");
+        }, 1000 * 2);
+      }, 1000 * 1);
+    },
+    [activeQuestionIndex]
+  );
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
   if (quizIsComplete) {
     return (
@@ -23,12 +48,6 @@ export default function Quiz() {
     );
   }
 
-  // event handlers
-  const handleSelectAnswer = useCallback((selectedAnswer) => {
-    setUserAnswers((prev) => [...prev, selectedAnswer]);
-  }, []);
-  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
-
   const suffledAnswers = QUESTIONS[activeQuestionIndex].answers.sort(() => Math.random() - 0.5);
 
   return (
@@ -41,14 +60,31 @@ export default function Quiz() {
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id='answers'>
-          {suffledAnswers.map((answer) => (
-            <li
-              key={answer}
-              className='answer'
-            >
-              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-            </li>
-          ))}
+          {suffledAnswers.map((answer) => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let classes = "";
+
+            if (answerState === "answered" && isSelected) {
+              classes += "selected";
+            }
+            if ((answerState === "correct" || answerState === "wrong") && isSelected) {
+              classes += answerState;
+            }
+
+            return (
+              <li
+                key={answer}
+                className='answer'
+              >
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={classes}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
