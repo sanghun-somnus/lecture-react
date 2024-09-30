906bd22025f152ec77e8a2e4669d875b0d19441c
sanghun-somnus <somnus.btc@gmail.com>
2024-09-30 14:27:47 +0900

    lecture: memo()로 컴포넌트 함수 실행 방지
    memo는 이전 props와 새로운 props를 비교한 후 값이 다른 경우 컴포넌트 함수를 실행하도록 한다
    
    불필요한 함수 실행을 방지한다고 모든 컴포넌트를 또는 props값이 빈번하게 변하는 컴포넌트를
    memo 함수로 래핑하는 것은 좋지 않다. 왜냐하면 props값을 비교하는 행위가 성능에
    영향을 줄 수 있기 때문이다

--- section13/src/components/Counter/Counter.jsx
+++ section13/src/components/Counter/Counter.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react';
+import { useState, memo } from "react";
 
-import IconButton from '../UI/IconButton.jsx';
-import MinusIcon from '../UI/Icons/MinusIcon.jsx';
-import PlusIcon from '../UI/Icons/PlusIcon.jsx';
-import CounterOutput from './CounterOutput.jsx';
-import { log } from '../../log.js';
+import IconButton from "../UI/IconButton.jsx";
+import MinusIcon from "../UI/Icons/MinusIcon.jsx";
+import PlusIcon from "../UI/Icons/PlusIcon.jsx";
+import CounterOutput from "./CounterOutput.jsx";
+import { log } from "../../log.js";
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log("Calculating if is prime number", 2, "other");
   if (number <= 1) {
     return false;
   }
@@ -27,8 +23,8 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
-  log('<Counter /> rendered', 1);
+const Counter = memo(function Counter({ initialCount }) {
+  log("<Counter /> rendered", 1);
   const initialCountIsPrime = isPrime(initialCount);
 
   const [counter, setCounter] = useState(initialCount);
@@ -42,20 +38,27 @@ export default function Counter({ initialCount }) {
   }
 
   return (
-    <section className="counter">
-      <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+    <section className='counter'>
+      <p className='counter-info'>
+        The initial counter value was <strong>{initialCount}</strong>. It <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
       </p>
       <p>
-        <IconButton icon={MinusIcon} onClick={handleDecrement}>
+        <IconButton
+          icon={MinusIcon}
+          onClick={handleDecrement}
+        >
           Decrement
         </IconButton>
         <CounterOutput value={counter} />
-        <IconButton icon={PlusIcon} onClick={handleIncrement}>
+        <IconButton
+          icon={PlusIcon}
+          onClick={handleIncrement}
+        >
           Increment
         </IconButton>
       </p>
     </section>
   );
-}
+});
+
+export default Counter;
