191543ca24c7263bd55dcee274a4809f6a43da17
sanghun-somnus <somnus.btc@gmail.com>
2024-09-30 16:28:27 +0900

    lecture: useCallback() 훅 이해하기

--- section13/src/components/UI/IconButton.jsx
+++ section13/src/components/UI/IconButton.jsx
@@ -1,13 +1,20 @@
-import { log } from '../../log.js';
+import { memo } from "react";
 
-export default function IconButton({ children, icon, ...props }) {
-  log('<IconButton /> rendered', 2);
+import { log } from "../../log.js";
+
+const IconButton = memo(function IconButton({ children, icon, ...props }) {
+  log("<IconButton /> rendered", 2);
 
   const Icon = icon;
   return (
-    <button {...props} className="button">
-      <Icon className="button-icon" />
-      <span className="button-text">{children}</span>
+    <button
+      {...props}
+      className='button'
+    >
+      <Icon className='button-icon' />
+      <span className='button-text'>{children}</span>
     </button>
   );
-}
+});
+
+export default IconButton;
