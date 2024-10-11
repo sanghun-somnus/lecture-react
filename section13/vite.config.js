e3f606d4b1c7f459af87d8a55628a473613122ba
sanghun-somnus <somnus.btc@gmail.com>
2024-10-11 18:59:23 +0900

    lecture: MillionJS

--- section13/vite.config.js
+++ section13/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import react from "@vitejs/plugin-react";
+import MillionLint from "@million/lint";
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [MillionLint.vite(), react()],
+});
