7b7539cbcafe951c7fd82079df3f7c4b12873b4a
sanghun-somnus <somnus.btc@gmail.com>
2024-10-25 03:42:31 +0900

    lecture: HTTP 에러 다루기

--- section15/src/components/AvailablePlaces.jsx
+++ section15/src/components/AvailablePlaces.jsx
@@ -1,22 +1,43 @@
 import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     setIsFetching(true);
     (async () => {
-      const resData = await (await fetch("http://localhost:3000/places")).json();
-      setAvailablePlaces(resData.places);
-      setIsFetching(false);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const data = await response.json();
 
-      console.log("[AvailablePlaces]", resData);
+        if (!response.ok) {
+          throw new Error("Faild to fetch places");
+        }
+        setAvailablePlaces(data.places);
+        // test message
+        console.log("[AvailablePlaces]", data);
+      } catch (error) {
+        setError({ message: error.message || "Could not fetch places, please try again later" });
+        // test message
+        console.error(error);
+      }
+      setIsFetching(false);
     })();
   }, []);
 
+  if (error) {
+    return (
+      <Error
+        title='An error occurred!'
+        message={error.message}
+      />
+    );
+  }
   return (
     <Places
       title='Available Places'
