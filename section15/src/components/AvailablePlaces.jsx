c283f1a41db3d6a57d8ce209397efa5afda4fe5f
sanghun-somnus <somnus.btc@gmail.com>
2024-10-24 18:12:20 +0900

    lecture: HTTP 요청을 보내지 않는 방법 (잘못된 이유 부가 설명)

--- section15/src/components/AvailablePlaces.jsx
+++ section15/src/components/AvailablePlaces.jsx
@@ -5,6 +5,14 @@ import Places from "./Places.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [AvailablePlaces, setAvailablePlaces] = useState([]);
 
+  fetch("http://localhost:3000/places")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setAvailablePlaces(data.places);
+    });
+
   return (
     <Places
       title='Available Places'
