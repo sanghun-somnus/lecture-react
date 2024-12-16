07dbb96567a96285691ae1fc18a83cde27d6aa99
sanghun-somnus <somnus.btc@gmail.com>
2024-12-16 15:08:50 +0900

    (243) Creating Custom Hook
    커스텀 훅을 정의할 때 폴더구조, 파일이름은 상관없다
    하지만, 꼭 지켜야하는 사항은 use 로 시작하는 함수이름을
    작성하는 것이다. 왜냐하면 React 자체에서 정한 Convention

--- section16/eslint.config.js
+++ section16/eslint.config.js
@@ -1,12 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact, { rules } from "eslint-plugin-react";
+import pluginHooks from "eslint-plugin-react-hooks";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginHooks.configs.recommended,
   {
     rules: {
       "react/jsx-uses-react": "off",
