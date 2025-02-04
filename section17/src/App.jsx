3672d1664aaca6580469dc65ef896fd08e5a2b1d
Kim Sanghun <sanghun-kim@naver.com>
2025-02-05 00:10:12 +0900

    Lecture: 258. Form 제출시 입력 유효성 검사
    키입력 방식의 유효성 검사를 진행하더라도
    제출방식의 유효성 검사도 추가하는 것이 좋다

--- section17/src/App.jsx
+++ section17/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header.jsx";
 import Signup from "./components/Signup.jsx";
+import Login from "./components/Login.jsx";
 import StateLogin from "./components/StateLogin.jsx";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
       <Header />
       <main>
         {/* <Signup /> */}
-        <StateLogin />
+        {/* <StateLogin /> */}
+        <Login />
       </main>
     </>
   );
--- section17/src/components/Login.jsx
+++ section17/src/components/Login.jsx
@@ -1,16 +1,35 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [isInvalid, setIsInvalid] = useState({
+    email: false,
+    password: false,
+  });
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    console.dir(email.current);
-    console.log(email.current.value);
-    console.dir(password.current);
-    console.log(password.current.value);
+    const isInvalidEmail = !email.current.value.includes("@");
+    const isInvalidPassword = password.current.value.length < 8;
+
+    if (isInvalidEmail) {
+      setIsInvalid((prev) => ({
+        ...prev,
+        email: true,
+      }));
+    }
+    if (isInvalidPassword) {
+      setIsInvalid((prev) => ({
+        ...prev,
+        password: true,
+      }));
+    }
+
+    if (!isInvalidEmail && !isInvalidPassword) {
+      console.log("send HTTP request...");
+    }
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -26,6 +45,9 @@ export default function Login() {
             placeholder="enter your email"
             ref={email}
           />
+          <div className="control-error">
+            {isInvalid.email && <p>must include @</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
@@ -37,6 +59,9 @@ export default function Login() {
             placeholder="enter your password"
             ref={password}
           />
+          <div className="control-error">
+            {isInvalid.password && <p>at least 8</p>}
+          </div>
         </div>
       </div>
 
